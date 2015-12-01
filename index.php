7df5033f57fdbbe8d379e0f95b8a45b1abb44344
Thomas Kjellberg <Thomas@Thomass-MacBook-Pro.local>
2015-12-01 15:52:33 +0100

    autoload removed

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php 
 // IMPORT CLASS FILES
-function __autoload($classname) {
+/*function __autoload($classname) {
     include $classname . '.class.php';
-}
+}*/
+
+require_once 'classes/User.class.php';
+require_once 'classes/Post.class.php';
+require_once 'classes/Comment.class.php';
+require_once 'classes/PagePrinter.class.php';
+require_once 'classes/Statistics.class.php';
 
 // Start a session 
 session_start();
