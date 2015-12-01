4dbbcc4ed04670256288546b403b5922b1919ee5
rickisen <rickard.spam@gmail.com>
2015-12-01 14:12:57 +0100

    Initial PagePrinter

---
+++ classes/PagePrinter.class.php
@@ -0,0 +1,22 @@
+<?php
+// INCLUDING TWIG
+require_once 'twig/lib/Twig/Autoloader.php';
+Twig_Autoloader::register();
+
+
+// SEND TO INDEX
+echo $twig -> render('index.html', $data);
+
+Class PagePrinter {
+  private $data = array(), $loader, $twig, $target = 'templates/blogg.twig';
+
+  function __construct($data){
+    $this->data   = $data;
+    $this->loader = new Twig_Loader_Filesystem('templates/');
+    $this->twig   = new Twig_Environment($loader);
+  }
+
+  function render(){
+    return $this->twig->render( $this->target, $this->data );
+  }
+}
