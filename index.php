3285f713f8934763d72d3f9b75c009bd84775821
carlowski <carl@carlgranstrom.se>
2015-12-01 12:01:06 +0100

    Added comments

--- index.php
+++ index.php
@@ -1,16 +1,21 @@
 <?php session_start();
 
+
+// INCLUDING TWIG
 require_once 'Twig/lib/Twig/Autoloader.php';
 Twig_Autoloader::register();
 $loader = new Twig_Loader_Filesystem('templates/');
 $twig = new Twig_Environment($loader);
 
+// CREATE TWIG TAGS TO RENDER IN HTML INDEX
 $data = [
 
 ];
 
+// SEND TO INDEX
 echo $twig -> render('index.html', $data);
 
+// IMPORT CLASS FILES
 function __autoload($classname) {
     include $classname . '.class.php';
 }
