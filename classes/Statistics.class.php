444bebb9273fe186737755d339566e5ec53ad038
carlowski <carl@carlgranstrom.se>
2015-12-02 13:35:21 +0100

    Basic class set up for Statistics

---
+++ classes/Statistics.class.php
@@ -0,0 +1,23 @@
+<?php
+
+class Statistics {
+	private $count_total_comments, $count_total_posts, $avg_comment_post, $three_most_commented_posts;
+
+	function __construct($count_total_comments, $count_total_posts, $avg_comment_post, $three_most_commented_posts) {
+		$this->count_total_comments = $count_total_comments;
+		$this->count_total_posts 	= $count_total_posts;
+		$this->avg_comment_post		= $three_most_commented_posts;
+	}
+
+	function __get($stats) {
+		$this->$stats;
+	}
+
+	function __isset($stats) {
+	    if (isset($this->$stats)){
+	        return true;
+	      } else {
+	        return false;
+	      }
+	    }
+}
