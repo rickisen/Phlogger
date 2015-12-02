12783ed0d283a38746160ea51eabcd1bfeb9cb2c
rickisen <rickard.spam@gmail.com>
2015-12-02 15:09:53 +0100

    made a new index.php to use the objects to laod the data

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php 
-// IMPORT CLASS FILES
-/*function __autoload($classname) {
-    include $classname . '.class.php';
-}*/
 
 require_once 'classes/UserSession.class.php';
 require_once 'classes/Post.class.php';
@@ -13,17 +9,30 @@ require_once 'classes/Statistics.class.php';
 // Start a session 
 session_start();
 
-$loadview = 'landingpage';
+// Check if we got a login request
+if (isset($_POST['username']) && isset($_POST['password']) && !isset($_SESSION['user'])){
+  $_SESSION['user'] = new UserSession($_POST['username'], $_POST['password']);  // escaped in the user class constructor
+} elseif (isset($_POST['logout'])) {
+  unsset($_SESSION['user']);
+}
+
+// Different default pages load depending on if we are loged in
+if ( isset($_SESSION['user']) ){
+  $loadview = 'dash';
+} else {
+  $loadview = 'landingpage';
+}
+// But if we get a explicit request we load that instead
 if (isset($_GET['loadview'])) {
 	$loadview = $_GET['loadview'];
 }
 
-$data = [
-	'loadview' => $loadview
-];
+// grabs the posts,comments,tags from the data 
+// base and makes objects from them
+$dataBase = new DataPuller();
 
-$view = new PagePrinter($data);
-
-echo $view->render();
+// create and render the twig-templates 
+$page = new PagePrinter( ['user' => $_SESSION['user'], 'dataBase' => $dataBase ]);
+echo $page->render();
 
 //echo $loadview;
