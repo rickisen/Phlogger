9ff89e1d36e2c83ebf2460d2802e69bc6c5ccf88
carlowski <carl@carlgranstrom.se>
2015-12-02 13:39:04 +0100

    Added one more line to constructor

--- classes/Statistics.class.php
+++ classes/Statistics.class.php
@@ -4,9 +4,10 @@ class Statistics {
 	private $count_total_comments, $count_total_posts, $avg_comment_post, $three_most_commented_posts;
 
 	function __construct($count_total_comments, $count_total_posts, $avg_comment_post, $three_most_commented_posts) {
-		$this->count_total_comments = $count_total_comments;
-		$this->count_total_posts 	= $count_total_posts;
-		$this->avg_comment_post		= $three_most_commented_posts;
+		$this->count_total_comments 	  = $count_total_comments;
+		$this->count_total_posts 		  = $count_total_posts;
+		$this->avg_comment_post 		  = $three_most_commented_posts;
+		$this->three_most_commented_posts = $three_most_commented_posts;
 	}
 
 	function __get($stats) {
