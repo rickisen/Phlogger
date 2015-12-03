1a5444297b52d73593562a84bf6ff2b5aebd3eb9
rickisen <rickard.spam@gmail.com>
2015-12-03 19:54:20 +0100

    Added a property and a block in constructor to group the posts by 12 in an array

--- classes/DataPuller.class.php
+++ classes/DataPuller.class.php
@@ -1,6 +1,6 @@
 <?php
 class DataPuller{
-  private $posts = array(), $statistics;
+  private  $groupedPosts = array(), $posts = array(), $statistics;
 
   // query to get all posts
   private $qAllPosts = 'SELECT * FROM post ORDER BY Timestamp';
@@ -14,6 +14,21 @@ class DataPuller{
     while ($row = $result->fetch_assoc()) {
       $this->posts[] = new Post($row['Title'], $row['Content'], $row['Author'], 
                                    $row['Timestamp'], $row['id']);
+    }
+
+    // Divide the posts into groups of 12, so that 
+    // we don't fill the landingpage
+    $limit = 12 ;
+    $step = 0 ;
+    $group = 0 ;
+    foreach ( $this->posts as $post )  {
+      if ($step++ < $limit ){
+        $this->groupedPosts[$group][] = $post;
+      } else {
+        $limit = 0;
+        ++$group;
+        $this->groupedPosts[$group][] = $post;
+      }
     }
 
     // Create a statistics object
