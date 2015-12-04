75cea38095c1837dcfeb3038308f08990b7f1da1
rickisen <rickard.spam@gmail.com>
2015-12-04 12:25:20 +0100

    Added ability to up a post to the database

--- classes/Post.class.php
+++ classes/Post.class.php
@@ -48,17 +48,46 @@ class Post{
 
   function getComments(){
     $database = new mysqli('localhost', 'root', '','Phlogger');
+
     $qAllComments = '
       SELECT *
       FROM comment 
       WHERE comment.post = '.$this->id.'
     ';
 
-    $result = $database->query($qAllComments);
-    while ($row = $result->fetch_assoc()) {
-      $this->comments[] = new Comment($row['Content'], $row['Signature'], $row['Date'] );
+    if( $result = $database->query($qAllComments) ) {
+      while ($row = $result->fetch_assoc()) {
+        $this->comments[] = new Comment($row['Content'], $row['Signature'], $row['Date'] );
+      }
+    } else {
+      echo "Error when trying to get a comment: ".$database->error;
+      return FALSE;
     }
 
     return $this->comments;
   }
+
+  function storePost(){
+    $database = new mysqli('localhost', 'root', '','Phlogger');
+
+    // escape the input before upping
+    $title     = $this->mysqli->real_escape_string($this->title);
+    $content   = $this->mysqli->real_escape_string($this->content);
+    $signature = $this->mysqli->real_escape_string($this->signature);
+
+    $upQuery = 'INSERT INTO post (Content, Author, title) 
+      VALUES ('.$content.', '.$author.', '.$title.' )';
+
+    // send the dml query to the db and save the responce
+    $responce = $database->query($upQueryA);
+
+    // lazy error reporting
+    if ($responce) {
+      return TRUE; // it worked!
+    } else {
+      echo "Error when trying to upload a post: ".$database->error;
+      return FALSE;
+    }
+  }
+
 }
