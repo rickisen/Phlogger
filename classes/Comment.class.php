f2338835d3027fd9bf46598bb7520bfa62d5e3fc
rickisen <rickard.spam@gmail.com>
2015-12-04 18:04:57 +0100

    Added ability to store a comment

--- classes/Comment.class.php
+++ classes/Comment.class.php
@@ -16,4 +16,27 @@ class Comment{
   function __isset($name){
     return isset($this->$name);
   }
+
+  function storeComment($postID){
+    $database = new mysqli('localhost', 'root', '','Phlogger');
+
+    // escape the input before upping
+    $content    = $database->real_escape_string($this->content);
+    $signature  = $database->real_escape_string($this->signature);
+    $postID     = $database->real_escape_string($postID);
+
+    $upQuery = 'INSERT INTO comment (Content, Signature, post ) 
+      VALUES (\''.$content.'\', \''.$signature.'\',\''.$postID.'\')';
+
+    // send the dml query to the db and save the responce
+    $response = $database->query($upQuery);
+
+    // lazy error reporting
+    if ($response) {
+      return TRUE; // it worked!
+    } else {
+      echo "Error when trying to upload a Comment: ".$database->error;
+      return FALSE;
+    }
+  }
 }
