79e4ea9f9c42e6e8b111635724a2f52ad6f1322c
rickisen <rickard.spam@gmail.com>
2015-12-04 14:38:57 +0100

    we can no longer get the contents of password from the usersession

--- classes/UserSession.class.php
+++ classes/UserSession.class.php
@@ -1,41 +1,46 @@
 <?php
 
 class UserSession {
-	private $username, $password, $rank, $isLoggedIn = FALSE, $mysqli;
+  private $username, $password, $rank, $isLoggedIn = FALSE, $mysqli;
 
-	function __construct ($username, $password) {
-		$this->mysqli = new mysqli('localhost', 'root', '','Phlogger');
-                // stripslashes: Returns a string with backslashes stripped off. 
-                // (\' becomes ' and so on.) Double backslashes (\\) 
-                // are made into a single backslash (\).
-		/* $username = $this->mysqli->stripslashes($username); */  
-		/* $password = $this->mysqli->stripslashes($password); */
+  function __construct ($username, $password) {
+          $this->mysqli = new mysqli('localhost', 'root', '','Phlogger');
+          // stripslashes: Returns a string with backslashes stripped off. 
+          // (\' becomes ' and so on.) Double backslashes (\\) 
+          // are made into a single backslash (\).
+          /* $username = $this->mysqli->stripslashes($username); */  
+          /* $password = $this->mysqli->stripslashes($password); */
 
-		$this->username = $this->mysqli->real_escape_string($username);
-		$this->password = $this->mysqli->real_escape_string($password);
+          $this->username = $this->mysqli->real_escape_string($username);
+          $this->password = $this->mysqli->real_escape_string($password);
 
-		$this->login();
-	}
+          $this->login();
+  }
 
-	function login() {
-		$query = 'SELECT Username, password, Rank FROM user';
+  function login() {
+      $query = 'SELECT Username, password, Rank FROM user';
 
-		$result = $this->mysqli->query($query);
-                                
-        while ( $row = $result->fetch_assoc() ) {
-            if ($row['Username'] == $this->username && $row['password'] == $this->password){
-	                $this->rank = $row['Rank'];
-	                $this->isLoggedIn = TRUE;
-            } 
-        }	
-	}
+      $result = $this->mysqli->query($query);
+                      
+      while ( $row = $result->fetch_assoc() ) {
+          if ($row['Username'] == $this->username && $row['password'] == $this->password){
+                      $this->rank = $row['Rank'];
+                      $this->isLoggedIn = TRUE;
+          } 
+      }	
+  }
 
-    function __get($x){
+  function __get($x){
+    if ( $x != 'password' )
       return $this->$x;
+    else {
+      echo 'Hacking damages your health';
+      return FALSE;
     }
+  }
 
-	function __isset($x){
-	  return isset($this->$x);
-	}
+  function __isset($x){
+    return isset($this->$x);
+  }
 
 }
