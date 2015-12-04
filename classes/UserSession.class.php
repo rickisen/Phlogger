15659a1b3ef65da12dfec88a3d5cd7ebcb6bf209
rickisen <rickard.spam@gmail.com>
2015-12-04 16:55:31 +0100

    Added support so we save the id here

--- classes/UserSession.class.php
+++ classes/UserSession.class.php
@@ -1,15 +1,10 @@
 <?php
 
 class UserSession {
-  private $username, $password, $rank, $isLoggedIn = FALSE, $mysqli;
+  private $username, $password, $id, $rank, $isLoggedIn = FALSE, $mysqli;
 
   function __construct ($username, $password) {
           $this->mysqli = new mysqli('localhost', 'root', '','Phlogger');
-          // stripslashes: Returns a string with backslashes stripped off. 
-          // (\' becomes ' and so on.) Double backslashes (\\) 
-          // are made into a single backslash (\).
-          /* $username = $this->mysqli->stripslashes($username); */  
-          /* $password = $this->mysqli->stripslashes($password); */
 
           $this->username = $this->mysqli->real_escape_string($username);
           $this->password = $this->mysqli->real_escape_string($password);
@@ -25,6 +20,7 @@ class UserSession {
       while ( $row = $result->fetch_assoc() ) {
           if ($row['Username'] == $this->username && $row['password'] == $this->password){
                       $this->rank = $row['Rank'];
+                      $this->id = $row['id'];
                       $this->isLoggedIn = TRUE;
           } 
       }	
