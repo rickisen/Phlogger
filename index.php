08ad072876c3aa07d28a85c7d8b54c06edb5aba9
Thomas <thomas.kjellberg@gmail.com>
2015-12-04 13:26:53 +0100

    templates updated and index.php tweaked

--- index.php
+++ index.php
@@ -10,6 +10,9 @@ require_once 'classes/Statistics.class.php';
 // Start a session 
 session_start();
 
+if (isset($_SESSION['user']) && !$_SESSION['user']->isLoggedIn) {
+	unset($_SESSION['user']);
+}
 // Check if we got a login request
 if (isset($_POST['username']) && isset($_POST['password'])){ //removed "!isset $_SESSION"
   $_SESSION['user'] = new UserSession($_POST['username'], $_POST['password']);  // escaped in the user class constructor
@@ -22,13 +25,18 @@ if (isset($_POST['username']) && isset($_POST['password'])){ //removed "!isset $
 // What happens if someone manualy puts in a get request for dash?!!
 //
 if ( isset($_SESSION['user']) && $_SESSION['user']->isLoggedIn  ) {
-  $loadview = 'dashboard';
+  $loadview = 'dash';
 } else {
   $loadview = 'landingpage';
 }
 // But if we get an explicit request we load that instead
 if (isset($_GET['loadview'])) {
 	$loadview = $_GET['loadview'];
+}
+
+$readmore = "";
+if (isset($_GET['readmore'])) {
+	$readmore = $_GET['readmore'];
 }
 
 // grabs the posts,comments,tags from the data 
@@ -38,8 +46,9 @@ $dataBase = new DataPuller();
 // create and render the twig-templates 
 if (isset($_SESSION['user'])){
   $page = new PagePrinter(['user' => $_SESSION['user'], 'dataBase' => $dataBase, 'loadview' => $loadview]);
-} else {
-  $page = new PagePrinter(['dataBase' => $dataBase, 'loadview' => $loadview ]);
+}
+else {
+	$page = new PagePrinter(['dataBase' => $dataBase, 'loadview' => $loadview, 'readmore' => $readmore ]);
 }
 
 echo $page->render();
