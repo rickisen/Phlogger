cba2237ab92d6b91545070a169f80bac1cb7a592
rickisen <rickard.spam@gmail.com>
2015-12-05 14:00:33 +0100

    Basically a rewrite for better structure and support for looking up tags

--- classes/DataPuller.class.php
+++ classes/DataPuller.class.php
@@ -1,30 +1,93 @@
 <?php
 class DataPuller{
-  private  $threePosts = array(), $groupedPosts = array(), $tags = array() ,$posts = array(), $statistics, $searchResults ;
+  private $threePosts, $groupedPosts, $tags, $posts, $statistics, $searchResults ;
+  private $database ; // where our connection to the database will live
 
-  // query to get all posts
-  private $qAllPosts = 'SELECT * FROM post join user on post.Author = user.id ORDER BY Timestamp DESC';
+  function __get($x){
+    return $this->$x;
+  }
 
-  // All tags
-  private $qAllTags = 'SELECT * FROM Tag ORDER BY id DESC';
+  function __isset($x){
+    return isset($this->$x);
+  }
 
   function __construct() { 
-    $database = new mysqli('localhost', 'root', '','Phlogger');
+    // Create the connection to our db
+    $this->database = new mysqli('localhost', 'root', '','Phlogger');
     
-    // Get all tags
-    $result = $database->query($this->qAllTags);
-    while ($row = $result->fetch_assoc()) {
-      $this->tags[] = new Tag( $row['Name'], $row['id'] );
+    $this->getTags();
+    $this->getAllPosts();
+    $this->getGroupedPosts();
+    $this->getThreePosts();
+
+    // Create a statistics object
+    $this->statistics = new Statistics();
+  }
+
+  function getPostsFromTag($tagID){
+    $qOnePost = ' 
+      SELECT post.* 
+      FROM   post LEFT JOIN p_Has_t
+        ON   p_Has_t.tagID = '.$tagID.'
+      ORDER BY post.Timestamp DESC
+    ';
+    $ret = array();
+    // Construct the posts
+    if( $result = $this->database->query($qOnePost)){
+      while ($row = $result->fetch_assoc()) {
+        $ret[] = new Post($row['Title'],    $row['Content'], $row['Image'], 
+                        $row['Username'], $row['id'],      $row['Timestamp'] );
+      }
+    } else {
+      echo "Failed to get posts from tag $tagID: ".$this->database->error;
     }
+    return $ret;
+  }
+
+  function getPost($postID){
+    $qOnePost = ' SELECT * FROM post WHERE post.id = '.$postID.' LIMIT 1';
 
     // Construct all posts
-    // Ignores tags for now
-    $result = $database->query($this->qAllPosts);
-    while ($row = $result->fetch_assoc()) {
-      $this->posts[] = new Post($row['Title'],    $row['Content'], $row['Image'], 
-                                $row['Username'], $row['id'],      $row['Timestamp'] );
+    if( $result = $this->database->query($qOnePost)){
+      while ($row = $result->fetch_assoc()) {
+        $ret = new Post($row['Title'],    $row['Content'], $row['Image'], 
+                        $row['Username'], $row['id'],      $row['Timestamp'] );
+      }
+    } else {
+      echo "Failed to get Post $postID: ".$this->database->error;
     }
+    return $ret;
+  }
 
+  function getAllTags(){
+    $qAllTags = 'SELECT * FROM Tag ORDER BY id DESC';
+
+    // Get all tags
+    if( $result = $this->database->query($qAllTags)){
+      while ($row = $result->fetch_assoc()) {
+        $this->tags[] = new Tag( $row['Name'], $row['id'] );
+      }
+    } else {
+      echo "Failed to get Tags: ".$this->database->error;
+    }
+  }
+
+  function getAllPosts(){
+    // query to get all posts
+    $qAllPosts = 'SELECT * FROM post join user on post.Author = user.id ORDER BY Timestamp DESC';
+
+    // Construct all posts
+    if( $result = $this->database->query($qAllPosts)){
+      while ($row = $result->fetch_assoc()) {
+        $this->posts[] = new Post($row['Title'],    $row['Content'], $row['Image'], 
+                                  $row['Username'], $row['id'],      $row['Timestamp'] );
+      }
+    } else {
+      echo "Failed to get Posts: ".$this->database->error;
+    }
+  }
+
+  function getGroupedPosts(){
     // Divide the posts into groups of 12, so that 
     // we don't fill the landingpage
     $limit = 12 ;
@@ -39,28 +102,19 @@ class DataPuller{
         $this->groupedPosts[$group][] = $post;
       }
     }
+    return $this->groupedPosts;
+  }
 
+  function getThreePosts(){
     // put the 3 latest posts into an easily obtainable array
     $i = 0;
     while ($i < 3){
-      $threePosts[] = $this->posts[$i++];
+      $this->threePosts[] = $this->posts[$i++];
     }
-
-    // Create a statistics object
-    $this->statistics = new Statistics();
-  }
-
-  function __get($x){
-    return $this->$x;
-  }
-
-  function __isset($x){
-    return isset($this->$x);
+    return $this->threePosts;
   }
 
   function search($searchFor){
-    $database = new mysqli('localhost', 'root', '','Phlogger');
-
     $searchQuery = '
       SELECT * FROM post join user on post.Author = user.id
       WHERE  content LIKE "%'.$searchFor.'%" OR content LIKE "%'.$searchFor.'" 
@@ -70,13 +124,13 @@ class DataPuller{
       ORDER BY Timestamp DESC 
     ';
 
-    if ( $result = $database->query($searchQuery)){
+    if ( $result = $this->database->query($searchQuery)){
       while ($row = $result->fetch_assoc()) {
         $this->searchResults[] = new Post($row['Title'],    $row['Content'], $row['Image'], 
                                           $row['Username'], $row['id'],      $row['Timestamp'] );
       }
     } else {
-      echo 'Something went wrong with the search: '.$database->error ;
+      echo 'Something went wrong with the search: '.$this->database->error ;
       return FALSE;
     }
     return TRUE;
